c3258d7c0158e736ef1e59157aaddfe87ecfa878
fanqiechaodan <1484119655@qq.com>
2025-05-16 22:51:17 +0800

    Day4 of Gee

--- Gee/main.go
+++ Gee/main.go
@@ -12,26 +12,40 @@ func main() {
 		ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 
-	r.GET("/hello", func(ctx *gee.Context) {
-		// expect /hello?name=fqcd
-		ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Query("name"), ctx.Path)
+	r.GET("/index", func(ctx *gee.Context) {
+		ctx.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
 
-	r.GET("/hello/:name", func(ctx *gee.Context) {
-		// expect /hello/geektutu
-		ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Param("name"), ctx.Path)
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(ctx *gee.Context) {
+			ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+
+		v1.GET("/hello", func(ctx *gee.Context) {
+			// expect /hello?name=fqcd
+			ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Query("name"), ctx.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(ctx *gee.Context) {
+			// expect /hello/fqcd
+			ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Param("name"), ctx.Path)
+		})
+
+		v2.POST("/login", func(ctx *gee.Context) {
+			ctx.JSON(http.StatusOK, gee.H{
+				"username": ctx.PostForm("username"),
+				"password": ctx.PostForm("password"),
+			})
+		})
+	}
 
 	r.GET("/assets/*filepath", func(ctx *gee.Context) {
 		ctx.JSON(http.StatusOK, gee.H{
 			"filepath:": ctx.Param("filepath"),
-		})
-	})
-
-	r.POST("/login", func(ctx *gee.Context) {
-		ctx.JSON(http.StatusOK, gee.H{
-			"username": ctx.PostForm("username"),
-			"password": ctx.PostForm("password"),
 		})
 	})
 
